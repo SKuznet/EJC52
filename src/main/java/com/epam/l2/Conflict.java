ed540e47c0fe371da29f0bf43bef7f8236f0127e
Sergey Kuznetsov <SKuznetsov@idbs.com>
2017-10-05 19:16:56 +0300

    add cat info

---
+++ src/main/java/com/epam/l2/Conflict.java
@@ -0,0 +1,10 @@
+package com.epam.l2;
+
+public class Conflict {
+    public static void main(String[] args) {
+        System.out.println("Cat has meat");
+        System.out.println("Cat has milk");
+        System.out.println("Cat has bread");
+        System.out.println("Cat has nothing");
+    }
+}
